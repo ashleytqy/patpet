717ca0d6683a14309acb67021acd401e5c084858
ashleytqy <ashleytqy@gmail.com>
2016-03-02 17:34:48 -0500

    added footer + cleaned up some js code

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
 $(window).load(function () {
-
-
-Crafty.init(500,350, document.getElementById('game'));
-
-
-var count = 0;
+	
+ Crafty.init(500,350, document.getElementById('game'));
+	
+ var count = 0;
 
  var myEntity = Crafty.e('2D, DOM, Mouse, Image')
 .image("cat.png")
-.bind('MouseMove', function(e){
- 	Crafty.log('mouse is moving', e);
- 	count += 1;
- 	Crafty.log(count);
-	if (count % 5 == 0 ) {
-	var bodyWidth = document.body.clientWidth;
-  var bodyHeight = document.body.clientHeight;
-  var randPosX = Math.floor((Math.random()*bodyWidth));
-  var randPosY = Math.floor((Math.random()*bodyHeight));
-  var image = document.createElement("img");
-  image.src = 'images/kenma.png';
-  image.style.position = "absolute";
-	image.style.top = randPosY + "px";
-	image.style.left = randPosX + "px";
+.bind('MouseMove', function (e){
 
-		  document.getElementById("wind").appendChild(image);
+ 		count += 1;
+
+		if (count % 10 == 0 ) {
+			var bodyWidth = window.innerWidth;
+  			var bodyHeight = window.innerHeight;
+  			var randPosX = Math.floor((Math.random()*bodyWidth));
+  			var randPosY = Math.floor((Math.random()*bodyHeight));
+  			var image = document.createElement("img");
+  			image.src = 'images/kenma.png';
+  			image.style.top = randPosY + "px";
+			image.style.left = randPosX + "px";
+  			image.style.position = "absolute";
+  			document.getElementById("screen").appendChild(image);
+  			
 		};
-});
-
-
+	});
 
 });
